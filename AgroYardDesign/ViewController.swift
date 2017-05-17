42adfce56783b7d10cd7dcf2ebce0f7956d235b6
Admin <ruslanpopesku95@gmail.com>
2017-05-17 15:03:02 +0300

    First commit

--- AgroYardDesign/ViewController.swift
+++ AgroYardDesign/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var defaultDistanceToTop = CGFloat(180)
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileImageViewTop: NSLayoutConstraint!
+    
+    @IBOutlet weak var ghostView: UIView!
+    @IBOutlet weak var ghostViewTop: NSLayoutConstraint!
+    
+    @IBOutlet weak var dataView: UIView!
+    @IBOutlet weak var dataViewTop: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setGhostView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setScrollView() {
+        scrollView.contentSize = CGSize(width: self.scrollView.frame.width, height: 900)
     }
-
-
+    
+    func setGhostView() {
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
+        ghostView.addGestureRecognizer(gestureRecognizer)
+        ghostView.isUserInteractionEnabled = true
+        
+        //ghostViewTop.constant = -1000
+    }
+    
+    func tap() {
+        var neededConstant = CGFloat()
+        if dataViewTop.constant == defaultDistanceToTop {
+            neededConstant = self.profileImageView.bounds.size.height
+        } else {
+            neededConstant = defaultDistanceToTop
+        }
+        
+        UIView.animate(withDuration: 0.5, animations: {
+            self.dataViewTop.constant = neededConstant
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    
+    
 }
 
