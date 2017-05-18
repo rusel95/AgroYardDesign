2816fa336c0a56b517c0eca857c1394def256a89
Admin <ruslanpopesku95@gmail.com>
2017-05-18 10:05:17 +0300

    Normal more or less version

--- AgroYardDesign/ViewController.swift
+++ AgroYardDesign/ViewController.swift
@@ -15,55 +15,54 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     
     @IBOutlet weak var profileImageView: UIImageView!
-    @IBOutlet weak var profileImageViewTop: NSLayoutConstraint!
-    
-    @IBOutlet weak var ghostView: UIView!
-    @IBOutlet weak var ghostViewTop: NSLayoutConstraint!
-    
-    @IBOutlet weak var dataView: UIView!
-    @IBOutlet weak var dataViewTop: NSLayoutConstraint!
+//
+//    @IBOutlet weak var ghostView: UIView!
+//    @IBOutlet weak var ghostViewTop: NSLayoutConstraint!
+//    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var contentViewVerticalToImage: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.delegate = self
-        
-        setGhostView()
+//        
+//        setGhostView()
         setScrollView()
     }
     
     func setScrollView() {
-        scrollView.contentSize = CGSize(width: self.scrollView.frame.width, height: dataView.bounds.height + ghostView.bounds.height)
+        scrollView.contentSize = contentView.bounds.size
         print(scrollView.contentSize)
     }
-    
-    func setGhostView() {
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
-        ghostView.addGestureRecognizer(gestureRecognizer)
-        ghostView.isUserInteractionEnabled = true
-        
-//        ghostViewTop.constant = -1000
-    }
-    
-    func tap() {
-        var neededConstant = CGFloat()
-        if dataViewTop.constant == defaultDistanceToTop {
-            neededConstant = self.profileImageView.bounds.size.height
-        } else {
-            neededConstant = defaultDistanceToTop
-        }
-        
-        UIView.animate(withDuration: 0.5, animations: {
-            self.dataViewTop.constant = neededConstant
-            self.view.layoutIfNeeded()
-        })
-        
-        print(scrollView.contentSize)
-    }
-    
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        print(scrollView.contentSize)
-    }
+//
+//    func setGhostView() {
+//        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
+//        ghostView.addGestureRecognizer(gestureRecognizer)
+//        ghostView.isUserInteractionEnabled = true
+//        
+////        ghostViewTop.constant = -1000
+//    }
+//    
+//    func tap() {
+//        var neededConstant = CGFloat()
+//        if dataViewTop.constant == defaultDistanceToTop {
+//            neededConstant = self.profileImageView.bounds.size.height
+//        } else {
+//            neededConstant = defaultDistanceToTop
+//        }
+//        
+//        UIView.animate(withDuration: 0.5, animations: {
+//            self.dataViewTop.constant = neededConstant
+//            self.view.layoutIfNeeded()
+//        })
+//        
+//        print(scrollView.contentSize)
+//    }
+//    
+//    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+//        print(scrollView.contentSize)
+//    }
     
 
 }
